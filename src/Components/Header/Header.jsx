13e221b60dd23cfbe120369dd29d4a62ed31072e
alhad-tm <alhadchikkutm@gmail.com>
2023-07-25 14:19:19 +0530

    making responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from "./Header.module.css"
 import Logo from "../../assets/Logo.svg"
 import Invic from "../../assets/INVICIOUS.svg"
+import Bars from "../../assets/bars.svg"
 import { Link } from 'react-scroll'
 
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
-    <div className={css.container}>
+    <div className={css.container} >
+     
       <div className={css.left}>
        <img src={Logo} alt="" />
        <img src={Invic} alt="" />
@@ -16,14 +21,24 @@ const Header = () => {
 
 
       <div className={css.right}>
-        <ul className={css.ulist}>
-            <li> <Link to="home" spy={true} smooth={true}> Home  </Link> </li>
-            <li> <Link to="about" spy={true} smooth={true}> About  </Link> </li>
-            <li> <Link to="products" spy={true} smooth={true}> Products and Services </Link> </li>
+    
+      {menuOpened === false && mobile === true ? (
+        <div onClick={() => setMenuOpened(true)}>
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.4rem", height: "1.1rem" }}
+          />
+        </div>
+      ) : (
+        <ul className={css.ulist} onClick={()=>setMenuOpened(false)}>
+            <li> <Link onClick={() => setMenuOpened(!menuOpened)} to="home" spy={true} smooth={true}> Home  </Link> </li>
+            <li> <Link onClick={() => setMenuOpened(false)} to="about" spy={true} smooth={true}> About  </Link> </li>
+            <li onClick={() => setMenuOpened(false)}> <Link onClick={() => setMenuOpened(false)} to="products" spy={true} smooth={true}> Products and Services </Link> </li>
             {/* <li> <Link to="home" spy={true} smooth={true}> Service  </Link> </li> */}
-            <li> <Link to="contact" spy={true} smooth={true}> Contact </Link> </li>
+            <li> <Link onClick={() => setMenuOpened(false)} to="contact" spy={true} smooth={true}> Contact </Link> </li>
           
-        </ul>
+        </ul> ) }
         
       </div>
     </div>
